374f77dc7abdcec91baacddfc80c756a5f36e9e2
Ezejosue <aezequiel56@gmail.com>
2021-11-29 14:18:40 -0600

    :memo: Functions call, apply and bind.

--- assets/index.js
+++ assets/index.js
@@ -4,6 +4,11 @@ import AutoPlay from "./plugins/AutoPlay.js";
 const video = document.querySelector("video");
 const button = document.getElementById("playButton");
 const buttonMute = document.getElementById("muteButton");
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
+const player = new MediaPlayer({
+  el: video,
+  plugins: [
+    //new AutoPlay()
+  ],
+});
 button.onclick = () => player.togglePlay();
 buttonMute.onclick = () => player.toggleMute();
